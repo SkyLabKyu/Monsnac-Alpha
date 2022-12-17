d44dbfe8d4911cb31345786d0f6f5e9c24353316
KyuSiik <jamesacusick@gmail.com>
2022-12-17 17:39:07 +0000

    Initial Push

---
+++ Objects/Scripts/Catt.gd
@@ -0,0 +1,17 @@
+extends KinematicBody2D
+
+
+export var mSpeed = 0
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+func _process(delta):
+	var direction = Vector2(0, 0)
+	
+	direction.x = float(int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")))
+	direction.y = float(int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up")))
+	
+	move_and_collide(direction)
