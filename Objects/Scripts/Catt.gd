5a8ba21017e0861e9e4fa6dd2ab5092380281320
KyuSiik <jamesacusick@gmail.com>
2022-12-30 18:47:15 +0000

    Updated animations
    Updated animatiions for your consideration

--- Objects/Scripts/Catt.gd
+++ Objects/Scripts/Catt.gd
@@ -3,10 +3,7 @@ extends KinematicBody2D
 
 export var mSpeed = 0
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
+onready var Sprite = get_node("Sprite")
 
 func _process(delta):
 	var direction = Vector2(0, 0)
@@ -14,4 +11,18 @@ func _process(delta):
 	direction.x = float(int(Input.is_action_pressed("ui_right")) - int(Input.is_action_pressed("ui_left")))
 	direction.y = float(int(Input.is_action_pressed("ui_down")) - int(Input.is_action_pressed("ui_up")))
 	
+
+	if(direction.x!=0):
+		Sprite.animation = "Side"
+	elif(direction.y>0):
+		Sprite.animation = "Down"
+	elif(direction.y<0):
+		Sprite.animation = "Up"
+	else:
+		Sprite.animation = "Idle"
+	
+	if(direction.x>0):
+		Sprite.flip_h = true
+	else:
+		Sprite.flip_h = false
 	move_and_slide(direction*mSpeed)
